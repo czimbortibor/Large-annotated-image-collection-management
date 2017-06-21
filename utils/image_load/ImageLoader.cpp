c01d28f254f7c30299b2944a14b0cb74df27a80b
czimbortibor <czimbortibor@gmail.com>
2017-06-22 01:44:56 +0300

    recursive directory read

--- utils/image_load/ImageLoader.cpp
+++ utils/image_load/ImageLoader.cpp
@@ -1,13 +1,10 @@
 #include "ImageLoader.hpp"
 
-ImageLoader::ImageLoader(const QString dirName, QStringList& imageNames,
+ImageLoader::ImageLoader(QStringList& imageNames,
 						 QList<GraphicsImage>& results,
-                         const cv::Size& size,
-                         ImageCollection& imageCollection,
-                         const QString& originalDirPath, QObject* parent) : QObject(parent) {
+						 const cv::Size& size,
+						 ImageCollection& imageCollection, QObject* parent) : QObject(parent) {
     _imageNames = &imageNames;
-    _dirName = dirName;
-    _originalDirPath = originalDirPath;
     _results = &results;
     _size = size;
     _imageCollection = &imageCollection;
@@ -17,24 +14,25 @@ void ImageLoader::run() {
     _running.testAndSetOrdered(0, 1);
     for (int i = 0; i < _imageNames->length(); ++i) {
         if (static_cast<int>(_running)) {
-            QString* fullFileName = new QString(_dirName + "/" + _imageNames->at(i));
-			if (fullFileName->endsWith(".gif")) {
+			//QString* fullFileName = new QString(_dirName + "/" + _imageNames->at(i));
+			QString* fileName = new QString(_imageNames->at(i));
+			if (fileName->endsWith(".gif")) {
 				continue;
 			}
-            QString* originalFileName = new QString(_originalDirPath + "/" + _imageNames->at(i));
+			//QString* originalFileName = new QString(_originalDirPath + "/" + _imageNames->at(i));
 			cv::Mat cvImage;
-            cvImage = cv::imread(fullFileName->toStdString());
+			cvImage = cv::imread(fileName->toStdString());
             if (cvImage.data == 0) {
                 continue;
             }
 			cv::Mat* cvResizedImg = new cv::Mat();
             cv::resize(cvImage, *cvResizedImg, _size);
 			GraphicsImage* image = new GraphicsImage(ImageConverter::Mat2QImage(*cvResizedImg),
-											   *fullFileName, *originalFileName);
+											   *fileName, *fileName);
 			image->mat.reset(cvResizedImg);
 			_results->append(*image);
-			_imageCollection->insert(cvResizedImg, fullFileName, originalFileName);
-			emit resultReady(_index, *fullFileName, *originalFileName);
+			_imageCollection->insert(cvResizedImg, fileName, fileName);
+			emit resultReady(_index, *fileName, *fileName);
 			++_index;
             }
         }
